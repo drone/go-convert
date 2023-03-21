8164f83a8f1f3d2b87db7c6d215ab39831b47029
Eoin McAfee <eoin_mcafee@hotmail.co.uk>
2023-03-21 15:39:31 +0000

    initial happy path for github converter to harness v2

--- convert/github/convert.go
+++ convert/github/convert.go
@@ -12,22 +12,32 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package github converts Github pipelines to Harness pipelines.
+// Package github converts GitHub pipelines to Harness pipelines.
 package github
 
 import (
 	"bytes"
-	"errors"
+	"fmt"
 	"io"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 
+	v1 "github.com/drone/go-convert/convert/github/yaml"
 	harness "github.com/drone/spec/dist/go"
 
 	"github.com/drone/go-convert/internal/store"
 	"github.com/ghodss/yaml"
 )
 
-// Converter converts a Github pipeline to a Harness
+// conversion context
+type context struct {
+	pipeline []*v1.Pipeline
+	stage    *v1.Pipeline
+}
+
+// Converter converts a GitHub pipeline to a Harness
 // v1 pipeline.
 type Converter struct {
 	kubeEnabled   bool
@@ -43,7 +53,7 @@ type Converter struct {
 	// stage  *github.Stage
 }
 
-// New creates a new Converter that converts a Github
+// New creates a new Converter that converts a GitHub
 // pipeline to a Harness v1 pipeline.
 func New(options ...Option) *Converter {
 	d := new(Converter)
@@ -74,16 +84,16 @@ func New(options ...Option) *Converter {
 
 // Convert downgrades a v1 pipeline.
 func (d *Converter) Convert(r io.Reader) ([]byte, error) {
-	// src, err := circle.Parse(r)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// d.config = src // push the github config to the state
-	// return d.convert()
-	return nil, errors.New("not implemented")
+	src, err := v1.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	return d.convert(&context{
+		pipeline: src,
+	})
 }
 
-// ConvertString downgrades a v1 pipeline.
+// ConvertBytes downgrades a v1 pipeline.
 func (d *Converter) ConvertBytes(b []byte) ([]byte, error) {
 	return d.Convert(
 		bytes.NewBuffer(b),
@@ -107,23 +117,58 @@ func (d *Converter) ConvertFile(p string) ([]byte, error) {
 	return d.Convert(f)
 }
 
-// converts converts a github pipeline pipeline.
-func (d *Converter) convert() ([]byte, error) {
+// converts a GitHub pipeline to Harness pipeline.
+func (d *Converter) convert(ctx *context) ([]byte, error) {
 
 	// create the harness pipeline
 	pipeline := &harness.Pipeline{
 		Version: 1,
-		// Default: convertDefault(d.config),
+		Stages:  []*harness.Stage{},
 	}
 
-	// for _, steps := range d.config.Pipelines.Default {
-	// 	if steps.Stage != nil {
-	// 		// TODO support for fast-fail
-	// 		d.stage = steps.Stage // push the stage to the state
-	// 		stage := d.convertStage()
-	// 		pipeline.Stages = append(pipeline.Stages, stage)
-	// 	}
-	// }
+	for _, from := range ctx.pipeline {
+		if from == nil {
+			continue
+		}
+
+		var actionJob *v1.Job
+		if from.Jobs != nil {
+			for _, job := range from.Jobs {
+				actionJob = &job
+				break
+			}
+		}
+
+		var cloneStage *harness.CloneStage
+		if actionJob != nil {
+			for _, step := range actionJob.Steps {
+				cloneStage = convertClone(step)
+				if cloneStage != nil {
+					break
+				}
+			}
+		}
+
+		pipeline.Stages = append(pipeline.Stages, &harness.Stage{
+			Name: from.Name,
+			Type: "ci",
+			When: convertCond(&from.On),
+			Spec: &harness.StageCI{
+				Clone:    cloneStage,
+				Envs:     copyEnv(from.Environment),
+				Platform: convertRunsOn(actionJob.RunsOn),
+				Runtime: &harness.Runtime{
+					Type: "machine",
+					Spec: harness.RuntimeMachine{},
+				},
+				Steps: convertSteps(actionJob),
+				//Volumes:  convertVolumes(from.Volumes),
+
+				// TODO support for delegate.selectors from from.Node
+				// TODO support for stage.variables
+			},
+		})
+	}
 
 	// marshal the harness yaml
 	out, err := yaml.Marshal(pipeline)
@@ -132,4 +177,147 @@ func (d *Converter) convert() ([]byte, error) {
 	}
 
 	return out, nil
+}
+
+func convertClone(src *v1.Step) *harness.CloneStage {
+	if src == nil || !isCheckoutAction(src.Uses) {
+		return nil
+	}
+	dst := new(harness.CloneStage)
+	if src.With != nil {
+		if depth, ok := src.With["fetch-depth"]; ok {
+			dst.Depth, _ = toInt64(depth)
+		}
+	}
+	return dst
+}
+
+func convertCond(src *v1.WorkflowTriggers) *harness.When {
+	if src == nil || isTriggersEmpty(src) {
+		return nil
+	}
+
+	exprs := map[string]*harness.Expr{}
+
+	for eventName, eventCondition := range getEventConditions(src) {
+		if expr := convertEventCondition(eventCondition); expr != nil {
+			exprs[eventName] = expr
+		}
+	}
+
+	dst := new(harness.When)
+	dst.Cond = []map[string]*harness.Expr{exprs}
+	return dst
+}
+
+func getEventConditions(src *v1.WorkflowTriggers) map[string][]string {
+	eventConditions := make(map[string][]string)
+
+	if src.Push != nil {
+		eventConditions["push"] = src.Push.Branches
+	}
+	if src.PullRequest != nil {
+		eventConditions["pull_request"] = src.PullRequest.Branches
+	}
+	return eventConditions
+}
+
+func convertEventCondition(src []string) *harness.Expr {
+	if len(src) != 0 {
+		return &harness.Expr{In: src}
+	}
+	return nil
+}
+
+func isTriggersEmpty(src *v1.WorkflowTriggers) bool {
+	return (src.Push == nil || len(src.Push.Branches) == 0) &&
+		(src.PullRequest == nil || len(src.PullRequest.Branches) == 0)
+}
+
+func toInt64(value interface{}) (int64, error) {
+	switch v := value.(type) {
+	case int:
+		return int64(v), nil
+	case int64:
+		return v, nil
+	case float64:
+		return int64(v), nil
+	case string:
+		intValue, err := strconv.Atoi(v)
+		return int64(intValue), err
+	default:
+		return 0, fmt.Errorf("unsupported type for conversion to int64")
+	}
+}
+
+func isCheckoutAction(action string) bool {
+	matched, _ := regexp.MatchString(`^actions/checkout@`, action)
+	return matched
+}
+
+func convertRunsOn(src string) *harness.Platform {
+	if src == "" {
+		return nil
+	}
+	dst := new(harness.Platform)
+	switch {
+	case strings.Contains(src, "windows"), strings.Contains(src, "win"):
+		dst.Os = harness.OSWindows
+	case strings.Contains(src, "darwin"), strings.Contains(src, "macos"), strings.Contains(src, "mac"):
+		dst.Os = harness.OSDarwin
+	default:
+		dst.Os = harness.OSLinux
+	}
+	dst.Arch = harness.ArchAmd64 // we assume amd64 for now
+	return dst
+}
+
+// copyEnv returns a copy of the environment variable map.
+func copyEnv(src map[string]string) map[string]string {
+	dst := map[string]string{}
+	for k, v := range src {
+		dst[k] = v
+	}
+	return dst
+}
+
+func convertSteps(src *v1.Job) []*harness.Step {
+	var steps []*harness.Step
+	for _, step := range src.Steps {
+		if isCheckoutAction(step.Uses) {
+			continue
+		}
+		dst := &harness.Step{
+			Name: step.Name,
+		}
+
+		if step.Uses != "" {
+			dst.Name = step.Name
+			dst.Spec = convertAction(step)
+			dst.Type = "action"
+		} else {
+			dst.Name = step.Name
+			dst.Spec = convertRun(step)
+			dst.Type = "script"
+		}
+		steps = append(steps, dst)
+	}
+	return steps
+}
+
+func convertAction(src *v1.Step) *harness.StepAction {
+	dst := &harness.StepAction{
+		Uses: src.Uses,
+		With: src.With,
+		Envs: src.Environment,
+	}
+	return dst
+}
+
+func convertRun(src *v1.Step) *harness.StepExec {
+	dst := &harness.StepExec{
+		Run:  src.Run,
+		Envs: src.Environment,
+	}
+	return dst
 }
---
+++ convert/github/yaml/pipeline.go
@@ -0,0 +1,98 @@
+package yaml
+
+type Pipeline struct {
+	Name        string            `yaml:"name,omitempty"`
+	On          WorkflowTriggers  `yaml:"on,omitempty"`
+	Jobs        map[string]Job    `yaml:"jobs,omitempty"`
+	Environment map[string]string `yaml:"env,omitempty"`
+}
+
+type Event struct {
+	Branches []string `yaml:"branches,omitempty"`
+	Tags     []string `yaml:"tags,omitempty"`
+	Paths    []string `yaml:"paths,omitempty"`
+}
+
+type Job struct {
+	RunsOn      string            `yaml:"runs-on,omitempty"`
+	Steps       []*Step           `yaml:"steps,omitempty"`
+	Environment map[string]string `yaml:"env,omitempty"`
+}
+
+type Step struct {
+	Name        string                 `yaml:"name,omitempty"`
+	Uses        string                 `yaml:"uses,omitempty"`
+	With        map[string]interface{} `yaml:"with,omitempty"`
+	Run         string                 `yaml:"run,omitempty"`
+	If          string                 `yaml:"if,omitempty"`
+	Environment map[string]string      `yaml:"env,omitempty"`
+}
+
+type WorkflowTriggers struct {
+	Push                     *PushCondition                     `yaml:"push,omitempty"`
+	PullRequest              *PullRequestCondition              `yaml:"pull_request,omitempty"`
+	WorkflowDispatch         *WorkflowDispatchCondition         `yaml:"workflow_dispatch,omitempty"`
+	Schedule                 *ScheduleCondition                 `yaml:"schedule,omitempty"`
+	RepositoryDispatch       *RepositoryDispatchCondition       `yaml:"repository_dispatch,omitempty"`
+	IssueComment             *IssueCommentCondition             `yaml:"issue_comment,omitempty"`
+	Issues                   *IssuesCondition                   `yaml:"issues,omitempty"`
+	PullRequestReview        *PullRequestReviewCondition        `yaml:"pull_request_review,omitempty"`
+	PullRequestReviewComment *PullRequestReviewCommentCondition `yaml:"pull_request_review_comment,omitempty"`
+	Label                    *LabelCondition                    `yaml:"label,omitempty"`
+	Release                  *ReleaseCondition                  `yaml:"release,omitempty"`
+}
+
+type PushCondition struct {
+	Branches []string `yaml:"branches,omitempty"`
+	Tags     []string `yaml:"tags,omitempty"`
+	Paths    []string `yaml:"paths,omitempty"`
+}
+
+type PullRequestCondition struct {
+	Branches []string `yaml:"branches,omitempty"`
+	Tags     []string `yaml:"tags,omitempty"`
+	Paths    []string `yaml:"paths,omitempty"`
+	Types    []string `yaml:"types,omitempty"`
+}
+
+type WorkflowDispatchCondition struct {
+	Inputs map[string]InputDefinition `yaml:"inputs,omitempty"`
+}
+
+type InputDefinition struct {
+	Description string `yaml:"description,omitempty"`
+	Default     string `yaml:"default,omitempty"`
+	Required    bool   `yaml:"required,omitempty"`
+}
+
+type ScheduleCondition struct {
+	Cron string `yaml:"cron,omitempty"`
+}
+
+type RepositoryDispatchCondition struct {
+	Types []string `yaml:"types,omitempty"`
+}
+
+type IssueCommentCondition struct {
+	Types []string `yaml:"types,omitempty"`
+}
+
+type IssuesCondition struct {
+	Types []string `yaml:"types,omitempty"`
+}
+
+type PullRequestReviewCondition struct {
+	Types []string `yaml:"types,omitempty"`
+}
+
+type PullRequestReviewCommentCondition struct {
+	Types []string `yaml:"types,omitempty"`
+}
+
+type LabelCondition struct {
+	Types []string `yaml:"types,omitempty"`
+}
+
+type ReleaseCondition struct {
+	Types []string `yaml:"types,omitempty"`
+}
