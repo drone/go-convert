bf1dde8e96b221da50c781baa0d4abdef69cede5
Jamie Li <tianyi.li@harness.io>
2023-11-27 09:55:10 -0500

    add jenkins json converter

---
+++ convert/jenkinsjson/covert.go
@@ -0,0 +1,105 @@
+// Copyright 2022 Harness, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package jenkinsjson converts jenkinsjson pipelines to Harness pipelines.
+package jenkinsjson
+
+import (
+	"bytes"
+	"io"
+	"os"
+
+	jenkinsjson "github.com/drone/go-convert/convert/jenkinsjson/json"
+	"github.com/drone/go-convert/internal/store"
+)
+
+// conversion context
+type context struct {
+	// config *jenkinsjson.Pipeline
+	// job    *jenkinsjson.Job
+}
+
+// Converter converts a jenkinsjson pipeline to a Harness
+// v1 pipeline.
+type Converter struct {
+	kubeEnabled   bool
+	kubeNamespace string
+	kubeConnector string
+	dockerhubConn string
+	identifiers   *store.Identifiers
+}
+
+// New creates a new Converter that converts a jenkinsjson
+// pipeline to a Harness v1 pipeline.
+func New(options ...Option) *Converter {
+	d := new(Converter)
+
+	// create the unique identifier store. this store
+	// is used for registering unique identifiers to
+	// prevent duplicate names, unique index violations.
+	d.identifiers = store.New()
+
+	// loop through and apply the options.
+	for _, option := range options {
+		option(d)
+	}
+
+	// set the default kubernetes namespace.
+	if d.kubeNamespace == "" {
+		d.kubeNamespace = "default"
+	}
+
+	// set the runtime to kubernetes if the kubernetes
+	// connector is configured.
+	if d.kubeConnector != "" {
+		d.kubeEnabled = true
+	}
+
+	return d
+}
+
+// Convert downgrades a v1 pipeline.
+func (d *Converter) Convert(r io.Reader) ([]byte, error) {
+	return jenkinsjson.Parse(r)
+}
+
+// ConvertBytes downgrades a v1 pipeline.
+func (d *Converter) ConvertBytes(b []byte) ([]byte, error) {
+	return d.Convert(
+		bytes.NewBuffer(b),
+	)
+}
+
+// ConvertString downgrades a v1 pipeline.
+func (d *Converter) ConvertString(s string) ([]byte, error) {
+	return d.Convert(
+		bytes.NewBufferString(s),
+	)
+}
+
+// ConvertFile downgrades a v1 pipeline.
+func (d *Converter) ConvertFile(p string) ([]byte, error) {
+	f, err := os.Open(p)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	return d.Convert(f)
+}
+
+// converts converts a jenkinsjson pipeline to a Harness pipeline.
+func (d *Converter) convert(ctx *context) ([]byte, error) {
+
+	return nil, nil
+}
