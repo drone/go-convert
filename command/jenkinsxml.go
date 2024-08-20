4558084de168fd6ee2ff150af35539f0054bca61
Jim Sheldon <jim.sheldon@gmail.com>
2024-08-20 09:34:29 -0400

    add jenkinsxml command

---
+++ command/jenkinsxml.go
@@ -0,0 +1,121 @@
+// Copyright 2024 Harness, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package command
+
+import (
+	"context"
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/drone/go-convert/convert/harness/downgrader"
+	"github.com/drone/go-convert/convert/jenkinsxml"
+
+	"github.com/google/subcommands"
+)
+
+type JenkinsXml struct {
+	name       string
+	proj       string
+	org        string
+	repoName   string
+	repoConn   string
+	kubeName   string
+	kubeConn   string
+	dockerConn string
+
+	downgrade   bool
+	beforeAfter bool
+}
+
+func (*JenkinsXml) Name() string     { return "jenkinsxml" }
+func (*JenkinsXml) Synopsis() string { return "converts a jenkins job xml file" }
+func (*JenkinsXml) Usage() string {
+	return `jenkinsxml [-downgrade] [job.xml]
+`
+}
+
+func (c *JenkinsXml) SetFlags(f *flag.FlagSet) {
+	f.BoolVar(&c.downgrade, "downgrade", false, "downgrade to the legacy yaml format")
+	f.BoolVar(&c.beforeAfter, "before-after", false, "print the before and after")
+
+	f.StringVar(&c.org, "org", "default", "harness organization")
+	f.StringVar(&c.proj, "project", "default", "harness project")
+	f.StringVar(&c.name, "pipeline", "default", "harness pipeline name")
+	f.StringVar(&c.repoConn, "repo-connector", "", "repository connector")
+	f.StringVar(&c.repoName, "repo-name", "", "repository name")
+	f.StringVar(&c.kubeConn, "kube-connector", "", "kubernetes connector")
+	f.StringVar(&c.kubeName, "kube-namespace", "", "kubernets namespace")
+	f.StringVar(&c.dockerConn, "docker-connector", "", "dockerhub connector")
+}
+
+func (c *JenkinsXml) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+	path := f.Arg(0)
+
+	// if the user does not specify the path as
+	// a command line arg, assume the default path.
+	if path == "" {
+		path = "job.xml"
+	}
+
+	// open the jenkins xml file
+	before, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Println(err)
+		return subcommands.ExitFailure
+	}
+
+	// convert the pipeline yaml from the gitlab
+	// format to the harness yaml format.
+	converter := jenkinsxml.New(
+		jenkinsxml.WithDockerhub(c.dockerConn),
+		jenkinsxml.WithKubernetes(c.kubeName, c.kubeConn),
+	)
+	after, err := converter.ConvertBytes(before)
+	if err != nil {
+		log.Println(err)
+		return subcommands.ExitFailure
+	}
+
+	// downgrade from the v1 harness yaml format
+	// to the v0 harness yaml format.
+	if c.downgrade {
+		// downgrade to the v0 yaml
+		d := downgrader.New(
+			downgrader.WithCodebase(c.repoName, c.repoConn),
+			downgrader.WithDockerhub(c.dockerConn),
+			downgrader.WithKubernetes(c.kubeName, c.kubeConn),
+			downgrader.WithName(c.name),
+			downgrader.WithOrganization(c.org),
+			downgrader.WithProject(c.proj),
+		)
+		after, err = d.Downgrade(after)
+		if err != nil {
+			log.Println(err)
+			return subcommands.ExitFailure
+		}
+	}
+
+	if c.beforeAfter {
+		os.Stdout.WriteString("---\n")
+		os.Stdout.Write(before)
+		os.Stdout.WriteString("\n---\n")
+	}
+
+	os.Stdout.Write(after)
+
+	return subcommands.ExitSuccess
+}
