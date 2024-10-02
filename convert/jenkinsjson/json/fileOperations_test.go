e5413326af1e6368c96136a6c10e57ba14b5e454
vanisrikaniti <c_vanisri.kanithi@harness.io>
2024-10-01 23:55:11 -0400

    feat: [CI-14420]: FileOps migration: FileDeleteOperation support

--- convert/jenkinsjson/json/fileOperations_test.go
+++ convert/jenkinsjson/json/fileOperations_test.go
@@ -2,13 +2,21 @@ package json
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
 
+	harness "github.com/drone/spec/dist/go"
 	"github.com/google/go-cmp/cmp"
 )
+
+type testRunner struct {
+	name  string
+	input Node
+	want  *harness.Step
+}
 
 func TestConvertFileOpsDelete(t *testing.T) {
 	// Get the working directory
@@ -89,4 +97,85 @@ func TestConvertFileOpsDelete(t *testing.T) {
 			t.Log(diff)
 		}
 	}
+}
+
+func prepareFileOpsTest(t *testing.T, filename string, folderName string, step *harness.Step) testRunner {
+
+	workingDir, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("failed to get current working directory: %v", err)
+	}
+
+	jsonData, err := os.ReadFile(filepath.Join(workingDir, "../convertTestFiles/fileOps/"+folderName, filename+".json"))
+	if err != nil {
+		t.Fatalf("failed to read JSON file: %v", err)
+	}
+
+	var inputNode Node
+	if err := json.Unmarshal(jsonData, &inputNode); err != nil {
+		t.Fatalf("failed to decode JSON: %v", err)
+	}
+
+	return testRunner{
+		name:  filename,
+		input: inputNode,
+		want:  step,
+	}
+}
+
+func TestConvertFileOpsDeleteFunction(t *testing.T) {
+
+	var tests []testRunner
+	tests = append(tests, prepareFileOpsTest(t, "fileOpsDelete_snippet", "fileOpsDelete", &harness.Step{
+		Id:   "fileOperations128b17",
+		Name: "fileDeleteOperation",
+		Type: "script",
+		Spec: &harness.StepExec{
+			Image: "alpine",
+			Run:   string("rm -rf **/old-files/*.log"),
+		},
+	}))
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			operation := extractAnanymousOperation(tt.input)
+			got := ConvertFileDelete(tt.input, operation)
+
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Errorf("ConvertFileDelete() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func extractAnanymousOperation(currentNode Node) map[string]interface{} {
+	// Step 1: Extract the 'delegate' map from the 'parameterMap'
+	delegate, ok := currentNode.ParameterMap["delegate"].(map[string]interface{})
+	if !ok {
+		fmt.Println("Missing 'delegate' in parameterMap")
+	}
+
+	// Step 2: Extract the 'arguments' map from the 'delegate'
+	arguments, ok := delegate["arguments"].(map[string]interface{})
+	if !ok {
+		fmt.Println("Missing 'arguments' in delegate map")
+	}
+
+	// Step 3: Extract the list of anonymous operations
+	anonymousOps, ok := arguments["<anonymous>"].([]interface{})
+	if !ok {
+		fmt.Println("No anonymous operations found in arguments")
+	}
+	var extractedOperation map[string]interface{}
+	// Step 4: Iterate over each operation and handle based on the 'symbol' type
+	for _, op := range anonymousOps {
+		// Convert the operation to a map for easy access
+		operation, ok := op.(map[string]interface{})
+		if !ok {
+			fmt.Println("Invalid operation format")
+			continue
+		}
+		extractedOperation = operation
+	}
+	return extractedOperation
 }
