b3c4cf7a5e71e4dca33b5240d37f7b083371a832
Senthil Kumar T <c_senthil.kumar@harness.io>
2024-11-03 19:45:38 +0530

    adding slackSend go convert logic

---
+++ convert/jenkinsjson/json/slackSend.go
@@ -0,0 +1,24 @@
+package json
+
+import (
+	harness "github.com/drone/spec/dist/go"
+)
+
+var JenkinsToDroneSlackSendParamMapperList = []JenkinsToDroneParamMapper{
+	{"channel", "channel", StringType, nil},
+	{"tokenCredentialId", "access_token", StringType, nil},
+	{"username", "username", StringType, nil},
+	{"color", "color", StringType, nil},
+	{"message", "message", StringType, nil},
+	{"iconEmoji", "icon_emoji", StringType, nil},
+	// "teamDomain" e.g https://<teamDomain>.slack.com - Unsupported Jenkins field
+}
+
+func ConvertSlackSend(node Node, variables map[string]string) *harness.Step {
+
+	step := ConvertToStepWithProperties(&node, variables, JenkinsToDroneSlackSendParamMapperList,
+		SlackPluginImage)
+
+	return step
+
+}
