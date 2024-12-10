448f8f538c2fba1bca010f53bee3cff993f98c92
c_senthil.kumar@harness.io <c_senthil.kumar@harness.io>
2024-12-10 12:08:13 +0530

    adding slackUserIdFromEmail conversion logic

--- convert/jenkinsjson/convert.go
+++ convert/jenkinsjson/convert.go
@@ -665,6 +665,9 @@ func collectStepsWithID(currentNode jenkinsjson.Node, stepWithIDList *[]StepWith
 	case "slackUploadFile":
 		*stepWithIDList = append(*stepWithIDList, StepWithID{Step: jenkinsjson.ConvertSlackUploadFile(currentNode, variables), ID: id})
 
+	case "slackUserIdFromEmail":
+		*stepWithIDList = append(*stepWithIDList, StepWithID{Step: jenkinsjson.ConvertSlackUserIdFromEmail(currentNode, variables), ID: id})
+
 	case "nexusArtifactUploader":
 		*stepWithIDList = append(*stepWithIDList, StepWithID{Step: jenkinsjson.ConvertNexusArtifactUploader(currentNode, variables), ID: id})
 
